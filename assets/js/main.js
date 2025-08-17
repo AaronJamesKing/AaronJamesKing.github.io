29ecd8dd8888b1d70a3924e1f6592a820da7bb3c
Aaron King <aaron@aaronjamesking.com>
2025-08-16 20:03:02 -0400

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const INTRO_MESSAGE = `
+👋 welcome to Root Software! This is Aaron King, I run the company.
+
+Since you're inspecting the console logs, I'll assume you're a developer of some kind.
+
+A big part of Root Software is supporting all developers professionally however they need it.
+
+If you're looking for work, whether it's contract-based or FTE, please get in touch. We do both subcontracting and recruiting.
+
+You can email me at aaron@rootsoftware.dev or connect on LinkedIn (https://www.linkedin.com/in/aaron-james-king/).
+
+Happy Coding! 😃
+`;
+
+console.log(INTRO_MESSAGE);
